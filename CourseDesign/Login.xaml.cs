915a15376eaa72c2a01028c277d850b3367c5932
Night1611 <night@loli.gs>
2019-02-02 13:47:13 +0800

    add login page

--- CourseDesign/Login.xaml.cs
+++ CourseDesign/Login.xaml.cs
@@ -17,8 +17,8 @@ namespace CourseDesign {
 	/// <summary>
 	/// MainWindow.xaml 的交互逻辑
 	/// </summary>
-	public partial class MainWindow : Window {
-		public MainWindow() {
+	public partial class Login : Window {
+		public Login() {
 			InitializeComponent();
 		}
 
